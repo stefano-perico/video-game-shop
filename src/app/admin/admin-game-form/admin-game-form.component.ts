421ab971e5ca7168e9a572b2d1fa15663ec5cfa6
stefano-perico <wap58@mail.com>
2019-10-07 15:14:51 +0200

    create newGame Form

--- src/app/admin/admin-game-form/admin-game-form.component.ts
+++ src/app/admin/admin-game-form/admin-game-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+import {GamesService} from '../../services/games.service';
+import {Game} from '../../models/game.model';
 
 @Component({
   selector: 'app-admin-game-form',
@@ -7,9 +11,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminGameFormComponent implements OnInit {
 
-  constructor() { }
+  gameForm: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private gamesService: GamesService
+  ) { }
 
   ngOnInit() {
+    this.initForm();
+  }
+
+  initForm() {
+    this.gameForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      author: ['', Validators.required],
+      price: ['', [Validators.required, Validators.pattern('^[0-9]*$')]],
+      synopsis: ''
+    });
+  }
+
+  onSaveGame() {
+    const name = this.gameForm.get('name').value;
+    const author = this.gameForm.get('author').value;
+    const price = this.gameForm.get('price').value;
+    const synopsis = this.gameForm.get('synopsis').value;
+
+    const newGame = new Game(name, author, price);
+    newGame.synopsis = synopsis;
+
+    this.gamesService.createNewGames(newGame);
+    this.router.navigate(['/admin', 'books']);
   }
 
 }
